b51655f5ad13771cbd24d1ec11ef3ee8881791ba
jacky <xinjie.zhang@gmail.com>
2022-03-02 23:14:39 +0800

    Move keyframes and animation config to core

--- src/vms-core.js
+++ src/vms-core.js
@@ -44,6 +44,19 @@
                 black: '#000000',
                 transparent: 'transparent',
                 current: 'currentColor'
+            },
+            keyframes: {
+                spin: `{to{transform:rotate(360deg)}}`,
+                ping: `{75%,100%{transform:scale(2);opacity:0}}`,
+                pulse: `{50%{opacity:.5}}`,
+                bounce: `{0%,100%{transform:translateY(-25%);animation-timing-function:cubic-bezier(0.8,0,1,1)}50%{transform:none;animation-timing-function:cubic-bezier(0,0,0.2,1)}}`
+            },
+            animation: {
+                none: '',
+                spin: `spin 1s linear infinite`,
+                ping: `ping 1s cubic-bezier(0, 0, 0.2, 1) infinite`,
+                pulse: `pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite`,
+                bounce: `bounce 1s infinite`
             }
         }
     }
--- src/vms-paint.js
+++ src/vms-paint.js
@@ -180,19 +180,6 @@
         return `transition-delay: ${parts[1]}ms;`
     })
 
-    C.keyframes = {
-        spin: `{to{transform:rotate(360deg)}}`,
-        ping: `{75%,100%{transform:scale(2);opacity:0}}`,
-        pulse: `{50%{opacity:.5}}`,
-        bounce: `{0%,100%{transform:translateY(-25%);animation-timing-function:cubic-bezier(0.8,0,1,1)}50%{transform:none;animation-timing-function:cubic-bezier(0,0,0.2,1)}}`
-    }
-    C.animation = {
-        none: '',
-        spin: `spin 1s linear infinite`,
-        ping: `ping 1s cubic-bezier(0, 0, 0.2, 1) infinite`,
-        pulse: `pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite`,
-        bounce: `bounce 1s infinite`
-    }
     const ani = `animate-`
     R(ani, classDetails => {
         let k = classDetails.name.substring(ani.length)
